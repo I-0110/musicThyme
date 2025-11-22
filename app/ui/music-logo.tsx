d3e09dff288242bd76ec3b7a1d0bbdf5bbdb3981
IvelisBecker <ivelisbecker@gmail.com>
2025-11-22 07:22:35 -0600

    Adjust Music Logo in md screens

--- app/ui/music-logo.tsx
+++ app/ui/music-logo.tsx
@@ -6,7 +6,7 @@ export default function MusicLogo() {
     <div
       className={`${teachers.className} flex flex-row items-center leading-none text-white`}
     >
-      <MusicalNoteIcon className="h-6 w-6" />
+      <MusicalNoteIcon className="h-6 w-6 hidden md:block" />
       <p className="text-[32px]">Music Thyme!</p>
     </div>
   );
