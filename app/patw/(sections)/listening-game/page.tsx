3c8e5737ad30ffe7520067cc700158fca5f37fe8
IvelisBecker <ivelisbecker@gmail.com>
2025-12-05 07:10:21 -0600

    Added background image to peter and the wolf

--- app/patw/(sections)/listening-game/page.tsx
+++ app/patw/(sections)/listening-game/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
   return (
     <div className="flex flex-col">
         <Nav />
-        <div className="flex min-h-screen flex-col bg-white dark:bg-black p-6">
+        <div className="flex min-h-screen flex-col bg-[url('/forrest.jpg')] bg-cover bg-center p-6">
             <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
                 <div className='flex flex-col justify-center gap-4 rounded-lg bg-thyme-100 p-6 md:w-2/5 md:px-10'>
                     <p className={`${teachers.className} text-xl text-thyme-500 md:text-3xl md:leading-normal`}>
