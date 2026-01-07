7e9e80c7c6fe242ca4f9381cda8740e4ca8e3f39
IvelisBecker <ivelisbecker@gmail.com>
2026-01-07 15:46:47 -0600

    Comment CotA Listening Game

--- app/cota/page.tsx
+++ app/cota/page.tsx
@@ -21,12 +21,12 @@ export default function Page() {
                   >
                     <span>Explore our Map</span> <ArrowRightIcon className="w-5 md:w-6" />
                   </Link>
-                  <Link
+                  {/* <Link
                   href="/cota/listening-game"
                   className="flex items-center gap-5 self-start rounded-lg bg-carnival-blue px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-carnival-300 md:text-base"
                   >
                     <span>Start Listening Game</span> <ArrowRightIcon className="w-5 md:w-6" />
-                  </Link>
+                  </Link> */}
                 </div>
             </div>
         </div>
